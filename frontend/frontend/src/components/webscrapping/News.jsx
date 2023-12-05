d80ffe7cf6679536cb696ef52d0d3c7d73b691cf
Pomardinho <janpose04@gmail.com>
2023-12-05 18:59:31 +0100

    Added styles with Bootstrap

--- frontend/frontend/src/components/webscrapping/News.jsx
+++ frontend/frontend/src/components/webscrapping/News.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function News() {
+    document.title = "News"
     const [news, setNews] = useState([]);
-
+    
     useEffect(() => {
         fetch("http://localhost:8080/news").then(res => res.json()).then(data => {
             setNews(data.news)
@@ -11,16 +13,20 @@ function News() {
     }, [])
 
     return (
-        <div>
+        <div class="container text-light">
             <h1>News</h1>
-            {news.map((item, index) => (
-                <div key={index}>
-                    <a href={item.url} target="_blank" rel="noopener noreferrer">
-                        <img src={item.picture} alt={item.title} />
-                        <p>{item.title}</p>
-                    </a>
-                </div>
-            ))}
+            <div class="row">
+                {news.map((item, index) => (
+                    <div class="col-md-6 mb-3" key={index}>
+                        <div class="bg-black bg-opacity-25 rounded p-2">
+                            <img class="img-fluid rounded" src={item.picture} alt={item.title}/>
+                            <a class="link-primary" href={item.url} target="_blank" rel="noopener noreferrer">
+                                <p>{item.title}</p>
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- frontend/frontend/src/components/wordreference/SearchWordForm.jsx
+++ frontend/frontend/src/components/wordreference/SearchWordForm.jsx
@@ -3,6 +3,7 @@ import { defineWord } from 'wordreference'
 import DisplayTranslations from './DisplayTranslations'
 
 function SearchWordForm() {
+    document.title = "Wordreference"
     const [word, setWord] = useState("")
     const [language, setLanguage] = useState("Spanish")
     const [translations, setTranslations] = useState(null)
@@ -14,6 +15,14 @@ function SearchWordForm() {
         setTranslations(parseTranslations(result))
         setSubmitted(true)
     }
+
+    useEffect(() => {
+        if (submitted) {
+            SearchWord()
+        }
+
+        setSubmitted(false)
+    }, [submitted])
 
     const parseTranslations = (result) => {
         const parsedTranslations = []
@@ -39,28 +48,20 @@ function SearchWordForm() {
         return parsedTranslations
     }
 
-    useEffect(() => {
-        if (submitted) {
-            SearchWord()
-        }
-
-        setSubmitted(false)
-    }, [submitted])
-
     return (
-        <div>
+        <div class="container text-light">
             <h1>Search words in English and get the translation!</h1>
             <form onSubmit={SearchWord}>
-                <label>Translate </label>
-                <input type="text" value={word} onChange={(e) => setWord(e.target.value)} placeholder="any word" required/>
-                <label> to: </label>
-                <select value={language} onChange={(e) => setLanguage(e.target.value)}>
-                    <option value="Spanish">Spanish</option>
+                <label>Translate:</label>
+                <input class="bg-black bg-opacity-25 text-light border-0 rounded mx-2 ps-2 focus-ring focus-ring-secondary" type="text" value={word} onChange={(e) => setWord(e.target.value)} placeholder="any word" required/>
+                <label>to:</label>
+                <select class="bg-black bg-opacity-25 text-light border-0 rounded mx-2 ps-2 focus-ring focus-ring-secondary" value={language} onChange={(e) => setLanguage(e.target.value)}>
+                    <option class="bg-black bg-opacity-25" value="Spanish">Spanish</option>
                     <option value="French">French</option>
                     <option value="Italian">Italian</option>
                 </select>
                 <br/>
-                <button type="submit">Go!</button>
+                <button class="btn btn-success mb-3" type="submit">Go!</button>
             </form>
 
             {translations && (<DisplayTranslations translations={translations}/>)}
